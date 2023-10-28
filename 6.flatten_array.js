226835029c51f7f247c82d3f2296f523d8eb12aa
rajeshwarikadagad <rajkadagad625@gmail.com>
2023-10-28 18:39:26 +0530

    Update 6.flatten_array.js

--- 6.flatten_array.js
+++ 6.flatten_array.js
@@ -1,5 +1,6 @@
 // flatten the given nested array
 
+//-------------------------- method-1 ---------------------------------------
 
 
 const myArr = [1, 3, [2, [4, 5, 6], 7], 8];
@@ -8,3 +9,34 @@ const newArr = myArr.flat(2);
 
 console.log(newArr)
 // console.log(Math.max(...newArr)); 
+
+
+
+//-------------------------- method-2  Recursive Method---------------------------------------
+function flattenDeep(arr) {
+  return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+}
+
+// Example usage:
+const nestedArray = [1, [2, [3, 4, [5, 6]], 7], 8, [9, [10]]];
+const flattenedArray = flattenDeep(nestedArray);
+console.log(flattenedArray);
+
+//-------------------------- method-3 ---------------------------------------
+const nestedArray = [1, [2, [3, 4], 5], 6];
+const flattenedArray = nestedArray.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatten(val) : val), []);
+
+//-------------------------- method-4 ---------------------------------------
+
+const _ = require('lodash');
+const nestedArray = [1, [2, [3, 4], 5], 6];
+const flattenedArray = _.flattenDeep(nestedArray);
+//-------------------------- method-5 ---------------------------------------
+
+function flattenDeep(arr) {
+  return [].concat(...arr.map(item => (Array.isArray(item) ? flattenDeep(item) : item)));
+}
+const nestedArray = [1, [2, [3, 4], 5], 6];
+const flattenedArray = flattenDeep(nestedArray);
+
+
