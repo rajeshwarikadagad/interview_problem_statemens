82c152a3dfe0b16e0c4da3d1597dc841756529dc
rajeshwarikadagad <rajkadagad625@gmail.com>
2023-10-29 14:58:24 +0530

    Update 2.sort_given_array.js

--- 2.sort_given_array.js
+++ 2.sort_given_array.js
@@ -39,6 +39,25 @@ for (let i = 0; i < s.length;) {
 }
 console.log(s);
 
+// -------------Bubble sort
+Array.prototype.myArraySort = function() {
+  for (let i = 0; i < this.length - 1; i++) {
+    for (let j = 0; j < this.length - i - 1; j++) {
+      if (this[j] > this[j + 1]) {
+        // Swap elements if they are in the wrong order
+        const temp = this[j];
+        this[j] = this[j + 1];
+        this[j + 1] = temp;
+      }
+    }
+  }
+};
+
+// Example usage:
+const myArray = [3, 1, 4, 1, 5, 9, 2, 6, 5];
+myArray.myArraySort();
+console.log(myArray);
+
 
 /* method -4 using single for loop  Quick sort*/
 
