543945b6225033c310d2c7e26e571addde20d45d
rajeshwarikadagad <rajkadagad625@gmail.com>
2023-11-01 00:16:21 +0530

    Create 28.print_pattern.js

---
+++ 28.print_pattern.js
@@ -0,0 +1,13 @@
+function staircase(n) {
+    // Write your code here
+    for(let i=0; i<n; i++) {
+        let str ='';
+        for(let j=0; j<n; j++) {
+             if(j < n-i-1){
+               str += " ";
+             } else
+               str +="#";
+        }
+        console.log(str);
+    }
+}
